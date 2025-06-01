d1f1531603f0900b6daacd8fe9ce2d27d427eef9
Jerryhulass <jeremy.hulass@gmail.com>
2025-06-01 19:46:02 -0400

    finished Header and Banner for mobile

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,9 @@
 export default function Banner() {
     return (
-        <section className="banner bg-blue-600 text-white p-4 flex flex-col items-center sm:flex-row">
+        <section className="banner bg-blue-600 text-white p-6 flex flex-col items-center sm:flex-row">
             <div className="gap-5 flex">
-                <img src="/src/assets/best-prices.svg" alt="best prices" />
-                <div>
+                <img src="/src/assets/best-prices.svg" alt="best prices" className=""/>
+                <div className="pb-5">
                     <h1>Best Prices</h1>
                     <p>All mechanical repairs and services are available at affordable rates</p>
                 </div>
@@ -11,7 +11,7 @@ export default function Banner() {
 
             <div className="gap-5 flex">
                 <img src="/src/assets/guarantee.svg" alt="guarantee" />
-                <div>
+                <div className="pb-5" >
                     <h1>100% Guarantee</h1>
                     <p>All of our repairs and services come with a guarantee period</p>
                 </div>
@@ -19,7 +19,7 @@ export default function Banner() {
 
             <div className="gap-5 flex">
                 <img src="/src/assets/certified.svg" alt="certified" />
-                <div>
+                <div className="pb-5">
                     <h1>Certified Mechanics</h1>
                     <p>Established in 1999, we bring over two decades of trusted auto-body experience.</p>
                 </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,22 @@
 export default function Header() {
   return (
-    <section className="bg-black text-white pt-0 p-4 items-center text-center">
+    <section className="bg-black text-white pt-0 p-4 items-center text-center flex sm:flex-row-reverse flex-col">
       
       <div id= "logo-address" className="flex flex-col items-center">
-        <img src="/src/assets/metro-body-logo.svg" alt="Metro Body & Fender Logo" className=""/>
-        <h1 className="text-2xl pt-0 pb-20 days-one-regular font-bold">619 Midland Ave, Garfield, NJ 07026</h1>
+        <img src="/src/assets/metro-body-logo.svg" alt="Metro Body & Fender Logo" className="sm:w-5xl"/>
+        <h1 className="text-2xl pt-0 pb-20 days-one-regular font-bold sm:text-4xl">619 Midland Ave, Garfield, NJ 07026</h1>
       </div>
 
       <div id="text-buttons" className="">
         <div id="text-header">
-          <h1 className="text-5xl font-bold days-one-regular leading-tight">Expert Car Repair Services </h1>
+          <h1 className="text-4xl font-bold days-one-regular leading-tight">Expert Car Repair Services </h1>
           <p className="text-xl pt-10 dm-sans-regular">We offering well-trained mechanics and excellent customer service, all at competitive prices of vehicle repairs at your home or place of work.</p>
         </div>
 
-        <div className="pt-8 days-one-regular"> 
-          <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">✉️ Contact Us</button>
-          <button className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">📞 201-398-0300</button>
+        <div className="pt-8 pb-8 days-one-regular flex space-x-4"> 
+          <button className="group relative h-12 overflow-hidden bg-blue-600 px-6 text-neutral-50 transition"><span>✉️ Contact Us</span><div className="absolute inset-0 h-full w-0 bg-white/30 transition-[width] group-hover:w-full"></div></button>
+
+          <button role="link" className="relative bg-[linear-gradient(#262626,#262626),linear-gradient(#3b82f6,#3b82f6)] bg-[length:100%_2px,0_2px] bg-[position:100%_100%,0_100%] bg-no-repeat text-white transition-[background-size,color] duration-500 hover:bg-[0_2px,100%_2px] hover:text-[#3b82f6]">📞 201-398-0300</button>
         </div>
 
       </div>
