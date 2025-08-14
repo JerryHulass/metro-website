c9dbeeb2b927a9cc40491578eceaa9d8eecc7a32
Jerryhulass <jeremy.hulass@gmail.com>
2025-08-14 17:59:51 -0400

    changed footer

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-400 text-center py-4 mt-auto">
-      © 2025 Jerry. All rights reserved.
+      © 2025 Metro Body and Fender. All rights reserved.
     </footer>
   );
 }
